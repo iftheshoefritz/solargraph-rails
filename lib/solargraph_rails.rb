142898a4f959a0aade628d93433fc2cf29c92988
Fritz Meissner <fritz.meissner@gmail.com>
2020-11-03 06:54:19 +0200

    First attempt at building convention

--- lib/solargraph_rails.rb
+++ lib/solargraph_rails.rb
@@ -1,5 +1,64 @@
-require "solargraph_rails/version"
+require 'solargraph_rails/version'
+require 'solargraph'
 
 module SolargraphRails
-  # Your code goes here...
+  class MyConvention < Solargraph::Convention::Base
+    def global yard_map
+      Solargraph::Logging.logger.info "*"*100 + 'running convention!'
+      puts "*"*100
+      Solargraph::Environ.new(pins: parse_pins)
+    end
+
+    private
+
+    def parse_pins
+      klasses = [MyBook] # use ActiveRecord::Base.descendants to find the real list
+      klass = ActiveRecord::Base.descendants.first
+      pins = []
+      ## TODO: find a way to do resolution that can handle namespaces:
+      source = Solargraph::Source.load(Rails.root.join('app', 'models', "#{klass.name.underscore}.rb"))
+      ## somehow this ^ line returns an error when I run `Solargraph::ApiMap.load(Rails.root)` in the console, but if I run the line itself directly in the console, no problem.
+
+      map = Solargraph::SourceMap.map(source)
+      class_location = map.first_pin(klass.name).location
+
+      # TODO: pins seems like it might not be a plain array
+      # in the example from CastWide which returned:
+      #
+      #     Solargraph::SourceMap.load_string(%(
+      #       class Foo; end
+      #     )).pins
+      #  Inside SourceMap it looks like pins is created like so:
+      #
+      #   @pins, @locals = Parser.map(source)
+      #
+      # TODO: what are the pins that Parser.map returns?
+      #         answer: returns [Array(Array<Pin::Base>, Array<Pin::Base>)]
+      # gem source: https://github.com/whitequark/parser/
+
+      pins << klass.columns.map do |col|
+        Solargraph::Pin::Method.new(
+          name: col.name,
+          comments: "@return [#{type_translation[col.type]}]",
+          location: class_location
+        )
+      end
+      puts pins
+      pins
+    end
+
+    def type_translation
+      {
+        decimal: 'Decimal',
+        integer: 'Int',
+        date: 'Date',
+        datetime: 'DateTime',
+        string: 'String',
+        boolean: 'Bool'
+      }
+    end
+  end
 end
+
+
+Solargraph::Convention.register MyConvention
--- solargraph_rails.gemspec
+++ solargraph_rails.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Fritz Meissner"]
   spec.email         = ["fritz.meissner@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Solargraph plugin that adds Rails-specific code through a Convention}
+  spec.description   = %q{Solargraph plugin that adds Rails-specific code through a Convention}
+  spec.homepage      = 'https://github.com/iftheshoefritz/solargraph_rails'
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -33,4 +33,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+
+  spec.add_runtime_dependency "solargraph", "~> 0.40.0"
 end
