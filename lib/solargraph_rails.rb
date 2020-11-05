f591d819889ba4c84810953622a0cbdad0336334
Fritz Meissner <fritz.meissner@gmail.com>
2020-11-05 07:33:04 +0200

    Move require to inside global method; fix logging

--- lib/solargraph_rails.rb
+++ lib/solargraph_rails.rb
@@ -5,7 +5,9 @@ require 'solargraph'
 module SolargraphRails
   class DynamicAttributes < Solargraph::Convention::Base
     def global yard_map
-      log :info, "*"*100 + 'running convention!'
+      log_message(:info, "#{'*'*100} running convention!")
+      log_message(:info, "#{'*'*25} attempting to require models")
+      Dir[Rails.root.join 'app', 'models', '*.rb')].each { |file| require file }
       Solargraph::Environ.new(pins: parse_pins)
     end
 
@@ -14,7 +16,7 @@ module SolargraphRails
     def parse_pins
       # TODO: use all descendants instead of the last
       klass = ActiveRecord::Base.descendants.select { |model_klass| !model_klass.abstract_class? }.first
-      log(:info, "*"*50 + "adding #{klass}")
+      log_message(:info, "#{'*'*50} + adding #{klass}")
       pins = []
       ## TODO: find a way to do resolution that can handle namespaces:
       source = Solargraph::Source.load(Rails.root.join('app', 'models', "#{klass.name.underscore}.rb"))
@@ -22,9 +24,9 @@ module SolargraphRails
       map = Solargraph::SourceMap.map(source)
       class_location = map.first_pin(klass.name).location
 
-      log(:info, klass.columns.map(&:name))
+      log_message(:info, klass.columns.map(&:name))
       klass.columns.each do |col|
-        log(:info, "*"*25 + "adding #{col.name}")
+        log_message(:info, "*"*25 + "adding #{col.name}")
         pins << Solargraph::Pin::Method.new(
           name: col.name,
           comments: "@return [#{type_translation[col.type]}]",
@@ -37,11 +39,11 @@ module SolargraphRails
       pins
     end
 
-    # log both to STDOUT and Solargraph logger while I am diagnosing from console
+    # log_message both to STDOUT and Solargraph logger while I am diagnosing from console
     # and client
-    def log(level, msg)
-      puts "[#{level}] msg"
-      Solargraph::Logging.logger.send(:level, msg)
+    def log_message(level, msg)
+      puts "[#{level}] #{msg}"
+      Solargraph::Logging.logger.send(level, msg)
     end
 
     def type_translation
