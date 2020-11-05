55ebe18982a13ba396c61b89156bd25132033460
Fritz Meissner <fritz.meissner@gmail.com>
2020-11-05 06:48:02 +0200

    Get plugin working with solargraph
    Still having problems with Rails loading

--- lib/solargraph_rails.rb
+++ lib/solargraph_rails.rb
@@ -1,51 +1,47 @@
+require 'rails/all'
 require 'solargraph_rails/version'
 require 'solargraph'
 
 module SolargraphRails
   class DynamicAttributes < Solargraph::Convention::Base
     def global yard_map
-      Solargraph::Logging.logger.info "*"*100 + 'running convention!'
-      puts "*"*100
+      log :info, "*"*100 + 'running convention!'
       Solargraph::Environ.new(pins: parse_pins)
     end
 
     private
 
     def parse_pins
-      klasses = [MyBook] # use ActiveRecord::Base.descendants to find the real list
-      klass = ApplicationRecord.descendants.first
-      Solargraph::Logging.logger.info "*"*50 + "adding #{klass}"
+      # TODO: use all descendants instead of the last
+      klass = ActiveRecord::Base.descendants.select { |model_klass| !model_klass.abstract_class? }.first
+      log(:info, "*"*50 + "adding #{klass}")
       pins = []
       ## TODO: find a way to do resolution that can handle namespaces:
       source = Solargraph::Source.load(Rails.root.join('app', 'models', "#{klass.name.underscore}.rb"))
-      ## somehow this ^ line returns an error when I run `Solargraph::ApiMap.load(Rails.root)` in the console, but if I run the line itself directly in the console, no problem.
 
       map = Solargraph::SourceMap.map(source)
       class_location = map.first_pin(klass.name).location
 
-      # TODO: pins seems like it might not be a plain array
-      # in the example from CastWide which returned:
-      #
-      #     Solargraph::SourceMap.load_string(%(
-      #       class Foo; end
-      #     )).pins
-      #  Inside SourceMap it looks like pins is created like so:
-      #
-      #   @pins, @locals = Parser.map(source)
-      #
-      # TODO: what are the pins that Parser.map returns?
-      #         answer: returns [Array(Array<Pin::Base>, Array<Pin::Base>)]
-      # gem source: https://github.com/whitequark/parser/
-
-      pins << klass.columns.map do |col|
-        Solargraph::Pin::Method.new(
+      log(:info, klass.columns.map(&:name))
+      klass.columns.each do |col|
+        log(:info, "*"*25 + "adding #{col.name}")
+        pins << Solargraph::Pin::Method.new(
           name: col.name,
           comments: "@return [#{type_translation[col.type]}]",
-          location: class_location
+          location: class_location,
+          scope: :instance,
+          attribute: true
         )
       end
       puts pins
       pins
+    end
+
+    # log both to STDOUT and Solargraph logger while I am diagnosing from console
+    # and client
+    def log(level, msg)
+      puts "[#{level}] msg"
+      Solargraph::Logging.logger.send(:level, msg)
     end
 
     def type_translation
