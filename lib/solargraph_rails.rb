edd8ec32899c3aa9f004bdf50ab738eaddf25f4e
Fritz Meissner <fritz.meissner@gmail.com>
2020-11-07 17:54:26 +0200

    Plugin now parses annotate instead of trying to load all of Rails
    Still lots of WIP

--- lib/solargraph_rails.rb
+++ lib/solargraph_rails.rb
@@ -1,42 +1,69 @@
-require 'rails/all'
 require 'solargraph_rails/version'
 require 'solargraph'
 
 module SolargraphRails
   class DynamicAttributes < Solargraph::Convention::Base
     def global yard_map
-      log_message(:info, "#{'*'*100} running convention!")
-      log_message(:info, "#{'*'*25} attempting to require models")
-      Dir[Rails.root.join 'app', 'models', '*.rb')].each { |file| require file }
-      Solargraph::Environ.new(pins: parse_pins)
+      puts "solarails caller stack:"
+      puts caller
+      log_message(:info, "******************************* running convention!")
+      Solargraph::Environ.new(pins: parse_models)
     end
 
     private
 
-    def parse_pins
-      # TODO: use all descendants instead of the last
-      klass = ActiveRecord::Base.descendants.select { |model_klass| !model_klass.abstract_class? }.first
-      log_message(:info, "#{'*'*50} + adding #{klass}")
+    def parse_models
       pins = []
-      ## TODO: find a way to do resolution that can handle namespaces:
-      source = Solargraph::Source.load(Rails.root.join('app', 'models', "#{klass.name.underscore}.rb"))
 
-      map = Solargraph::SourceMap.map(source)
-      class_location = map.first_pin(klass.name).location
+      klass_name = 'MyBook'
+      klass_name_underscore = 'my_book'
+      # doesn't like Rails.root here:
+      file_name = File.join(Dir.pwd, 'app', 'models', "#{klass_name_underscore}.rb")
 
-      log_message(:info, klass.columns.map(&:name))
-      klass.columns.each do |col|
-        log_message(:info, "*"*25 + "adding #{col.name}")
+      log_message :info, "loading from #{file_name}"
+
+      line_number = -1
+      File.open(file_name).each do |line|
+        line_number += 1
+        log_message :info, "PROCESSING: #{line}"
+        next if skip_line?(line)
+        break if end_comments?(line)
+        col_name, col_type = col_with_type(line)
+        log_message :info, "parsed name: #{col_name} type: #{col_type}"
+
+        loc = Solargraph::Location.new(file_name, Solargraph::Range.from_to(line_number, 0, line_number, line.length - 1))
+        log_message :info, loc.inspect
+
         pins << Solargraph::Pin::Method.new(
-          name: col.name,
-          comments: "@return [#{type_translation[col.type]}]",
-          location: class_location,
+          name: col_name,
+          comments: "@return [#{type_translation[col_type]}]",
+          location: loc,
+          closure: Solargraph::Pin::Namespace.new(name: klass_name),
           scope: :instance,
-          attribute: true
+          #attribute: true
         )
       end
-      puts pins
+      log_message(:info, pins)
+      log_message(:info, "*********** pin names:")
+      log_message(:info, pins.map(&:name))
       pins
+    end
+
+    def skip_line?(line)
+      skip = line.empty? || line =~ /Schema/ || line =~ /Table/ || line =~ /^\s*#\s*$/
+      log_message :info, 'skipping' if skip
+      skip
+    end
+
+    def end_comments?(line)
+      !line.start_with?('#')
+    end
+
+    def col_with_type(line)
+      line
+        .gsub(/#\s*/, '')
+        .split
+        .first(2)
     end
 
     # log_message both to STDOUT and Solargraph logger while I am diagnosing from console
