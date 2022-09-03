e70bf514ac33b36ec21dbd0d67f6529ea5849659
Stephen Sugden <me@stephensugden.com>
2022-09-03 10:21:17 +0200

    Move node matching methods to Walker::Hook

--- lib/solargraph/rails/walker.rb
+++ lib/solargraph/rails/walker.rb
@@ -2,11 +2,50 @@ module Solargraph
   module Rails
     class Walker
       class Hook
-        attr_reader :args, :proc, :node_type
+        attr_reader :node_type
         def initialize(node_type, args, &block)
           @node_type = node_type
           @args = args
           @proc = Proc.new(&block)
+        end
+
+        def visit(node)
+          return unless matches?(node)
+
+          if @proc.arity == 1
+            @proc.call(node)
+          elsif @proc.arity == 2
+            walker = Walker.new(node)
+            @proc.call(node, walker)
+            walker.walk
+          end
+        end
+
+        private
+
+        def matches?(node)
+          return unless node.type == node_type
+          return unless node.children
+          return true if @args.empty?
+
+          if node.children.first.is_a?(::Parser::AST::Node)
+            node.children.any? do |child|
+              child.is_a?(::Parser::AST::Node) &&
+                match_children(child.children, @args[1..-1])
+            end
+          else
+            match_children(node.children)
+          end
+        end
+
+        def match_children(children, args = @args)
+          args.each_with_index.all? do |arg, i|
+            if children[i].is_a?(::Parser::AST::Node)
+              children[i].type == arg
+            else
+              children[i] == arg
+            end
+          end
         end
       end
 
@@ -45,46 +84,9 @@ module Solargraph
       def traverse(node)
         return unless node.is_a?(::Parser::AST::Node)
 
-        @hooks[node.type].each { |hook| try_match(node, hook) }
+        @hooks[node.type].each { |hook| hook.visit(node) }
 
         node.children.each { |child| traverse(child) }
-      end
-
-      def try_match(node, hook)
-        return unless matches?(node, hook)
-
-        if hook.proc.arity == 1
-          hook.proc.call(node)
-        elsif hook.proc.arity == 2
-          walker = Walker.new(node)
-          hook.proc.call(node, walker)
-          walker.walk
-        end
-      end
-
-      def matches?(node, hook)
-        return unless node.type == hook.node_type
-        return unless node.children
-        return true if hook.args.empty?
-
-        if node.children.first.is_a?(::Parser::AST::Node)
-          node.children.any? do |child|
-            child.is_a?(::Parser::AST::Node) &&
-              match_children(hook.args[1..-1], child.children)
-          end
-        else
-          match_children(hook.args, node.children)
-        end
-      end
-
-      def match_children(args, children)
-        args.each_with_index.all? do |arg, i|
-          if children[i].is_a?(::Parser::AST::Node)
-            children[i].type == arg
-          else
-            children[i] == arg
-          end
-        end
       end
     end
   end
