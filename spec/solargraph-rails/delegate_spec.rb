2ac9cd062a4b927b4d342f1f12df5d610e9a8953
Stephen Sugden <grncdr@users.noreply.github.com>
2023-05-14 11:38:29 +0200

    Feature detect DelegatedMethod pin support

--- spec/solargraph-rails/delegate_spec.rb
+++ spec/solargraph-rails/delegate_spec.rb
@@ -1,25 +1,36 @@
 require 'spec_helper'
 
-RSpec.describe Solargraph::Rails::Delegate do
+skip_reason = 'Missing required Solargraph pin type' unless Solargraph::Rails::Delegate.supported?
+
+RSpec.describe Solargraph::Rails::Delegate, skip: skip_reason do
   let(:api_map) { Solargraph::ApiMap.new }
 
-  it "generates methods for singular association" do
+  it 'generates delegate method pins' do
     load_string 'app/thing.rb', <<-RUBY
       class Thing
         delegate :one, :two, to: :foo
         def foo
           Foo.new
         end
+
+        class Foo
+          def one
+            1
+          end
+
+          def two
+            "two"
+          end
+        end
       end
     RUBY
 
-    assert_public_instance_method(api_map, "Thing#one", ["undefined"])
-    assert_public_instance_method(api_map, "Thing#two", ["undefined"]) do |pin|
-      expect(pin.location.range.to_hash).to eq({
-        :start => { :line => 1, :character => 0 },
-        :end => { :line=>1, :character => 8 }
-      })
+    assert_public_instance_method(api_map, 'Thing#one', ['Integer'])
+    assert_public_instance_method(api_map, 'Thing#two', ['String']) do |pin|
+      expect(pin.location.range.to_hash).to eq(
+        start: { line: 1, character: 0 },
+        end: { line: 1, character: 8 }
+      )
     end
   end
 end
-
