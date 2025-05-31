a17f6060974dfee05ad6025f260a47496965a9c6
Vince Broz <vince@broz.cc>
2025-05-31 12:33:33 -0400

    Swap to comments method of specifying return type

--- lib/solargraph/rails/model.rb
+++ lib/solargraph/rails/model.rb
@@ -180,7 +180,8 @@ module Solargraph
               name = name[1..]
               decl = :restarg
             end
-            method.parameters << Solargraph::Pin::Parameter.new(name: name, decl: decl, closure: method, return_type: type)
+            method.parameters << Solargraph::Pin::Parameter.new(name: name, decl: decl, closure: method,
+                                                                comments: "@return [#{type}]")
           end
           pins << method
         end
