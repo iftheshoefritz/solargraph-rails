6573afcd7d4c23a9007d877131271afb324277fc
Vince Broz <vince@broz.cc>
2025-06-04 08:52:42 -0400

    Sync ActionController::Base includes

--- lib/solargraph/rails/annotations/action_controller.rb
+++ lib/solargraph/rails/annotations/action_controller.rb
@@ -1,12 +1,59 @@
 class ActionController::Base
-  include ActionController::MimeResponds
-  include ActionController::Redirecting
-  include ActionController::Cookies
+  #
+  # NOTE: keep this list synced with new items from MODULES in action_controller/base.rb
+  #
+  # @todo pull this as literal array dynamically from
+  #  ActionController::Base::MODULES and walk through it (and other
+  #  cases of same pattern) to help future-proof things
+  #
   include AbstractController::Rendering
+  extend AbstractController::Rendering::ClassMethods
+  include AbstractController::Translation
+  include AbstractController::AssetPaths
+  include Helpers
+  include UrlFor
+  include Redirecting
+  include ActionView::Layouts
+  include Rendering
+  include Renderers::All
+  include ConditionalGet
+  include EtagWithTemplateDigest
+  include EtagWithFlash
+  include Caching
+  include MimeResponds
+  include ImplicitRender
+  include StrongParameters
+  include ParameterEncoding
+  include Cookies
+  include Flash
+  include FormBuilder
+  include RequestForgeryProtection
+  extend RequestForgeryProtection::ClassMethods
+  include ContentSecurityPolicy
+  include PermissionsPolicy
+  include Streaming
+  include DataStreaming
+  include HttpAuthentication::Basic::ControllerMethods
+  extend HttpAuthentication::Basic::ControllerMethods::ClassMethods
+  include HttpAuthentication::Digest::ControllerMethods
+  include HttpAuthentication::Token::ControllerMethods
+  include DefaultHeaders
+  include Logging
+  AbstractController::Callbacks,
+  extend AbstractController::Callbacks::ClassMethods
+  include Rescue
+  include Instrumentation
+  include ParamsWrapper
+
+  #
+  # I don't see the thinsg below in action_controller/base.rb, at least in Rails
+  # 7.0.  Maybe they need to be moved to be under a different class?
+  #
   extend ActiveSupport::Callbacks::ClassMethods
   extend ActiveSupport::Rescuable::ClassMethods
-  extend AbstractController::Callbacks::ClassMethods
-  extend ActionController::RequestForgeryProtection::ClassMethods
+  include ActiveSupport::Rescuable
+
+>>>>>>> Stashed changes
 
   # @return [ActionDispatch::Response]
   def response; end
