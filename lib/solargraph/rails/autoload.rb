1949a3505322754d8bf821d45e8e864c6ad807cd
Vince Broz <vince@broz.cc>
2025-06-29 13:38:27 -0400

    Drop changed logs

--- lib/solargraph/rails/autoload.rb
+++ lib/solargraph/rails/autoload.rb
@@ -5,10 +5,6 @@ module Solargraph
         @instance ||= self.new
       end
 
-      def self.foo
-        false
-      end
-
       def process(source_map, ns, ds)
         return [] unless ds.size == 1 && ns.path.include?('::')
         Solargraph.logger.debug(
