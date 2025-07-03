8ef52f7db14352e20309266ee79071f28651de64
Vince Broz <vince@broz.cc>
2025-07-03 12:28:07 -0400

    Try adding reviews: write permission

--- lib/solargraph/rails/devise.rb
+++ lib/solargraph/rails/devise.rb
@@ -5,6 +5,10 @@ module Solargraph
         @instance ||= self.new
       end
 
+      def foo
+        DoesNotExist
+      end
+
       def process(source_map, ns)
         if Model.valid_filename?(source_map.filename)
           process_model(source_map.source, ns)
