be5a2323116829813e60144f1c557e9670330100
Vince Broz <vince@broz.cc>
2026-03-09 10:38:21 -0400

    Add annotations to fix skips in action_controller

--- lib/solargraph/rails/annotations/action_controller.rb
+++ lib/solargraph/rails/annotations/action_controller.rb
@@ -10,43 +10,71 @@ module ActionController
     include AbstractController::Rendering
     extend AbstractController::Rendering::ClassMethods
     include AbstractController::Translation
+    extend AbstractController::Translation::ClassMethods
     include AbstractController::AssetPaths
+    extend AbstractController::AssetPaths::ClassMethods
     include Helpers
+    extend Helpers::ClassMethods
     include UrlFor
+    extend UrlFor::ClassMethods
     include Redirecting
+    extend Redirecting::ClassMethods
     include ActionView::Layouts
+    extend ActionView::Layouts::ClassMethods
     include Rendering
+    extend Rendering::ClassMethods
     include Renderers::All
+    extend Renderers::All::ClassMethods
     include ConditionalGet
+    extend ConditionalGet::ClassMethods
     include EtagWithTemplateDigest
+    extend EtagWithTemplateDigest::ClassMethods
     include EtagWithFlash
+    extend EtagWithFlash::ClassMethods
     include Caching
+    extend Caching::ClassMethods
     include MimeResponds
+    extend MimeResponds::ClassMethods
     include ImplicitRender
+    extend ImplicitRender::ClassMethods
     include StrongParameters
+    extend StrongParameters::ClassMethods
     include ParameterEncoding
+    extend ParameterEncoding::ClassMethods
     include Cookies
+    extend Cookies::ClassMethods
     include Flash
+    extend Flash::ClassMethods
     include FormBuilder
+    extend FormBuilder::ClassMethods
     include RequestForgeryProtection
     extend RequestForgeryProtection::ClassMethods
     include ContentSecurityPolicy
+    extend ContentSecurityPolicy::ClassMethods
     include PermissionsPolicy
     extend PermissionsPolicy::ClassMethods
     include Streaming
+    extend Streaming::ClassMethods
     include DataStreaming
+    extend DataStreaming::ClassMethods
     include HttpAuthentication::Basic::ControllerMethods
     extend HttpAuthentication::Basic::ControllerMethods::ClassMethods
     include HttpAuthentication::Digest::ControllerMethods
+    extend HttpAuthentication::Digest::ControllerMethods::ClassMethods
     include HttpAuthentication::Token::ControllerMethods
+    extend HttpAuthentication::Token::ControllerMethods::ClassMethods
     include DefaultHeaders
+    extend DefaultHeaders::ClassMethods
     include Logging
     extend Logging::ClassMethods
     include AbstractController::Callbacks
     extend AbstractController::Callbacks::ClassMethods
     include Rescue
+    extend Rescue::ClassMethods
     include Instrumentation
+    extend Instrumentation::ClassMethods
     include ParamsWrapper
+    extend ParamsWrapper::ClassMethods
 
     #
     # I don't see the thinsg below in action_controller/base.rb, at least in Rails
